2b66ae99a854d45d9e87fe23cd3c34e7afddde6a
川辺 兼嗣 <kawanabe_kenji@grenge.co.jp>
2022-06-01 17:48:56 +0900

    敵モデルをデータに合わせて設置

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -6,16 +6,25 @@ namespace DungeonBuilder
 {
     public class EnemyManager : MonoBehaviour
     {
-        [SerializeField]
-        private EnemyView _enemyViewPrefab;
 
         private List<Enemy> _enemies;
 
         public List<Enemy> Enemies => _enemies;
 
+        [SerializeField]
+        private EnemyView _enemyViewPrefab;
+
+        private List<EnemyView> _enemyViews;
+
+        public List<EnemyView> EnemyViews => _enemyViews;
+
+        [SerializeField]
+        private Transform _viewParent;
+
         public void Initialize()
         {
             _enemies = new List<Enemy>();
+            _enemyViews = new List<EnemyView>();
         }
 
         public void PutMino(Mino mino)
@@ -25,6 +34,19 @@ namespace DungeonBuilder
 
             enemy.FieldPos = mino.FieldPos + enemy.FieldPos;
             Enemies.Add(enemy);
+
+            CreateEnemyView(enemy);
+        }
+
+        private void CreateEnemyView(Enemy enemy)
+        {
+            var enemyView = Instantiate(_enemyViewPrefab, _viewParent);
+            _enemyViews.Add(enemyView);
+
+            var worldPos = FieldView.GetWorldPosition(enemy.FieldPos) + Vector3.back;
+            enemyView.transform.localPosition = worldPos;
+
+            enemyView.lookAngles = Vector3.up * Random.Range(0, 3) * 90f;
         }
     }
 }
--- Assets/Scripts/EnemyView.cs
+++ Assets/Scripts/EnemyView.cs
@@ -6,5 +6,13 @@ namespace DungeonBuilder
 {
     public class EnemyView : MonoBehaviour
     {
+        [SerializeField]
+        private Transform _lookTransform;
+
+        public Vector3 lookAngles
+        {
+            get => _lookTransform.localEulerAngles;
+            set => _lookTransform.localEulerAngles = value;
+        }
     }
 }
